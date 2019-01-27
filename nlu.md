b5cddb5d0b1c0fedab1410f6c26a6837a4c415f1
Sarit Ritwirune <sarit@elcolie.com>
2019-01-27 19:19:32 +0700

    First try PyThaiNLP and RASA integration

--- nlu.md
+++ nlu.md
@@ -2,10 +2,14 @@
 ## intent:greet
 - hey
 - hello there
-
+- สวีดัด
+- หวัดดี
 
 ## intent:goodbye
 - cu
 - good by
 - cee you later
-
+- ลาก่อย
+- บาย
+- บ๊าย บาย
+- บัย
--- sentiment.py
+++ sentiment.py
@@ -3,193 +3,49 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from typing import Text
+
+from pythainlp.tokenize import word_tokenize
 from rasa_nlu.components import Component
+from rasa_nlu.tokenizers import Token
 
 
-class MyComponentA(Component):
-    """A new component"""
-
-    # Name of the component to be used when integrating it in a
-    # pipeline. E.g. ``[ComponentA, ComponentB]``
-    # will be a proper pipeline definition where ``ComponentA``
-    # is the name of the first component of the pipeline.
-    name = "CompA"
-
-    # Defines what attributes the pipeline component will
-    # provide when called. The listed attributes
-    # should be set by the component on the message object
-    # during test and train, e.g.
-    # ```message.set("entities", [...])```
-    provides = []
-
-    # Which attributes on a message are required by this
-    # component. e.g. if requires contains "tokens", than a
-    # previous component in the pipeline needs to have "tokens"
-    # within the above described `provides` property.
-    requires = []
-
-    # Defines the default configuration parameters of a component
-    # these values can be overwritten in the pipeline configuration
-    # of the model. The component should choose sensible defaults
-    # and should be able to create reasonable results with the defaults.
-    defaults = {}
-
-    # Defines what language(s) this component can handle.
-    # This attribute is designed for instance method: `can_handle_language`.
-    # Default value is None which means it can handle all languages.
-    # This is an important feature for backwards compatibility of components.
-    language_list = None
+class LextoTokenizer(Component):
+    """
+    Requires:
+    1. PyThaiNLP
+    2. sklearn_crfsuite
+    pip install https://github.com/PyThaiNLP/pythainlp/archive/dev.zip
+    pip install sklearn_crfsuite
+    """
+    name = "tokenizer_pylexto"
+    provides = ["tokens"]
 
     def __init__(self, component_config=None):
-        print('A')
-        import pdb; pdb.set_trace()
         super().__init__(component_config)
 
     def train(self, training_data, cfg, **kwargs):
-        """Train this component.
-
-        This is the components chance to train itself provided
-        with the training data. The component can rely on
-        any context attribute to be present, that gets created
-        by a call to :meth:`components.Component.pipeline_init`
-        of ANY component and
-        on any context attributes created by a call to
-        :meth:`components.Component.train`
-        of components previous to this one."""
-
         """
-        Access dataset and intents here
-        (Pdb) training_data.training_examples[0].text
-        'hey'
-        (Pdb) training_data.training_examples[0].data
-        {'intent': 'greet'}
+        No need any further training on this tokenizer
+        :param training_data:
+        :param cfg:
+        :param kwargs:
+        :return:
         """
-
-        print('A')
-        import pdb;
-        pdb.set_trace()
         pass
 
-    def process(self, message, **kwargs):
-        """Process an incoming message.
+    def process(self, message, **kwargs) -> None:
+        message.set("tokens", self.tokenize(message.text))
 
-        This is the components chance to process an incoming
-        message. The component can rely on
-        any context attribute to be present, that gets created
-        by a call to :meth:`components.Component.pipeline_init`
-        of ANY component and
-        on any context attributes created by a call to
-        :meth:`components.Component.process`
-        of components previous to this one."""
-        print('A')
-        import pdb;
-        pdb.set_trace()
-        pass
+    def tokenize(self, text: Text):
+        tmp = word_tokenize(text, engine='pylexto')
+        words = [elem for elem in tmp if elem.strip()]
 
-    def persist(self, model_dir):
-        """Persist this component to disk for future loading."""
-        print('A')
-        import pdb;
-        pdb.set_trace()
-        pass
-
-    @classmethod
-    def load(cls, model_dir=None, model_metadata=None, cached_component=None,
-             **kwargs):
-        """Load this component from file."""
-        print('A')
-        import pdb;
-        pdb.set_trace()
-        if cached_component:
-            return cached_component
-        else:
-            component_config = model_metadata.for_component(cls.name)
-            return cls(component_config)
-
-
-class MyComponentB(Component):
-    """A new component"""
-
-    # Name of the component to be used when integrating it in a
-    # pipeline. E.g. ``[ComponentA, ComponentB]``
-    # will be a proper pipeline definition where ``ComponentA``
-    # is the name of the first component of the pipeline.
-    name = "CompB"
-
-    # Defines what attributes the pipeline component will
-    # provide when called. The listed attributes
-    # should be set by the component on the message object
-    # during test and train, e.g.
-    # ```message.set("entities", [...])```
-    provides = []
-
-    # Which attributes on a message are required by this
-    # component. e.g. if requires contains "tokens", than a
-    # previous component in the pipeline needs to have "tokens"
-    # within the above described `provides` property.
-    requires = []
-
-    # Defines the default configuration parameters of a component
-    # these values can be overwritten in the pipeline configuration
-    # of the model. The component should choose sensible defaults
-    # and should be able to create reasonable results with the defaults.
-    defaults = {}
-
-    # Defines what language(s) this component can handle.
-    # This attribute is designed for instance method: `can_handle_language`.
-    # Default value is None which means it can handle all languages.
-    # This is an important feature for backwards compatibility of components.
-    language_list = None
-
-    def __init__(self, component_config=None):
-        import pdb;
-        pdb.set_trace()
-        super().__init__(component_config)
-
-    def train(self, training_data, cfg, **kwargs):
-        """Train this component.
-
-        This is the components chance to train itself provided
-        with the training data. The component can rely on
-        any context attribute to be present, that gets created
-        by a call to :meth:`components.Component.pipeline_init`
-        of ANY component and
-        on any context attributes created by a call to
-        :meth:`components.Component.train`
-        of components previous to this one."""
-        import pdb;
-        pdb.set_trace()
-        pass
-
-    def process(self, message, **kwargs):
-        """Process an incoming message.
-
-        This is the components chance to process an incoming
-        message. The component can rely on
-        any context attribute to be present, that gets created
-        by a call to :meth:`components.Component.pipeline_init`
-        of ANY component and
-        on any context attributes created by a call to
-        :meth:`components.Component.process`
-        of components previous to this one."""
-        import pdb;
-        pdb.set_trace()
-        pass
-
-    def persist(self, model_dir):
-        """Persist this component to disk for future loading."""
-        import pdb;
-        pdb.set_trace()
-        pass
-
-    @classmethod
-    def load(cls, model_dir=None, model_metadata=None, cached_component=None,
-             **kwargs):
-        """Load this component from file."""
-        import pdb;
-        pdb.set_trace()
-        if cached_component:
-            return cached_component
-        else:
-            component_config = model_metadata.for_component(cls.name)
-            return cls(component_config)
+        running_offset = 0
+        tokens = []
+        for word in words:
+            word_offset = text.index(word, running_offset)
+            word_len = len(word)
+            running_offset = word_offset + word_len
+            tokens.append(Token(word, word_offset))
+        return tokens
